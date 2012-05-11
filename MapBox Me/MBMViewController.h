99431f7778f3f3bb4223c8581e11243f9d6de763
Justin R. Miller <incanus@codesorcery.net>
2012-05-11 15:56:24 -0700

    handle nav control button with new SDK element

--- MapBox Me/MBMViewController.h
+++ MapBox Me/MBMViewController.h
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-#import "RMMapViewDelegate.h"
-
-@interface MBMViewController : UIViewController <RMMapViewDelegate>
+@interface MBMViewController : UIViewController
 
 @end
--- MapBox Me/MBMViewController.m
+++ MapBox Me/MBMViewController.m
@@ -10,19 +10,15 @@
 
 #import "RMMapView.h"
 #import "RMMapBoxSource.h"
-
-#define kMapBoxMeNormalTintColor [UIColor colorWithRed:0.120 green:0.650 blue:0.750 alpha:1.000]
-#define kMapBoxMeActiveTintColor [UIColor colorWithRed:0.120 green:0.550 blue:0.670 alpha:1.000]
+#import "RMUserTrackingBarButtonItem.h"
 
 @interface MBMViewController ()
 
 @property (nonatomic, strong) IBOutlet RMMapView *mapView;
 
-- (void)startTrackingLocation;
-- (void)startTrackingHeading;
-- (void)stopTracking;
-
 @end
+
+#pragma mark -
 
 @implementation MBMViewController
 
@@ -34,70 +30,26 @@
     
     self.title = @"MapBox Me";
     
-    self.navigationController.navigationBar.tintColor = kMapBoxMeActiveTintColor;
+    self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:0.120 green:0.550 blue:0.670 alpha:1.000];
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"TrackingLocation.png"]
-                                                                                                  style:UIBarButtonItemStyleBordered
-                                                                                                 target:self 
-                                                                                                 action:@selector(startTrackingLocation)];
+    self.navigationItem.rightBarButtonItem = [[RMUserTrackingBarButtonItem alloc] initWithMapView:self.mapView];
     
-    self.mapView.delegate = self;
+    self.navigationItem.rightBarButtonItem.tintColor = self.navigationController.navigationBar.tintColor;
+    
     self.mapView.tileSource = [[RMMapBoxSource alloc] initWithReferenceURL:[NSURL URLWithString:@"http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets.json"]];
     self.mapView.decelerationMode = RMMapDecelerationFast;
     self.mapView.adjustTilesForRetinaDisplay = YES;    
     self.mapView.centerCoordinate = CLLocationCoordinate2DMake(0, 0);
     self.mapView.minZoom = 1;
     self.mapView.zoom = 2;
-    
-    CGColorRef darkBackgroundColor = CGColorCreateCopyWithAlpha([self.navigationController.navigationBar.tintColor CGColor], 0.5);
-
-    self.mapView.backgroundColor = [UIColor colorWithCGColor:darkBackgroundColor];
-
-    CGColorRelease(darkBackgroundColor);
+    self.mapView.backgroundColor = [UIColor colorWithRed:0.120 green:0.550 blue:0.670 alpha:0.5];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
 
-    [self startTrackingLocation];
-}
-
-#pragma mark -
-
-- (void)startTrackingLocation
-{
-    self.navigationItem.rightBarButtonItem.image     = [UIImage imageNamed:@"TrackingLocation.png"];
-    self.navigationItem.rightBarButtonItem.tintColor = kMapBoxMeActiveTintColor;
-    self.navigationItem.rightBarButtonItem.action    = ([CLLocationManager headingAvailable] ? @selector(startTrackingHeading) : @selector(stopTracking));
-
     self.mapView.userTrackingMode = RMUserTrackingModeFollow;
-}
-
-- (void)startTrackingHeading
-{
-    self.navigationItem.rightBarButtonItem.image     = [UIImage imageNamed:@"TrackingHeading.png"];
-    self.navigationItem.rightBarButtonItem.tintColor = kMapBoxMeActiveTintColor;
-    self.navigationItem.rightBarButtonItem.action    = @selector(stopTracking);
-    
-    self.mapView.userTrackingMode = RMUserTrackingModeFollowWithHeading;
-}
-
-- (void)stopTracking
-{
-    self.navigationItem.rightBarButtonItem.image     = [UIImage imageNamed:@"TrackingLocation.png"];
-    self.navigationItem.rightBarButtonItem.tintColor = kMapBoxMeNormalTintColor;
-    self.navigationItem.rightBarButtonItem.action    = @selector(startTrackingLocation);
-
-    self.mapView.userTrackingMode = RMUserTrackingModeNone;    
-}
-
-#pragma mark -
-
-- (void)mapView:(RMMapView *)mapView didChangeUserTrackingMode:(RMUserTrackingMode)mode animated:(BOOL)animated
-{
-    if (mode == RMUserTrackingModeNone)
-        [self stopTracking];
 }
 
 @end
